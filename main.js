d97a560e10d889428be45a2a8ec636fc07b3dcb3
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2022-06-06 02:37:23 -0300

    adicionado código para corrigir erros relacionados ao menu

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-function onScroll() {
-  scrollY > 0 
-  ? navigation.classList.add('scroll') 
-  : navigation.classList.remove('scroll')
-}
+// --------------------------------
+// Antigo código que deu bug e não consegui resolver
+// function onScroll() {
+//   scrollY > 0
+//     ? navigation.classList.add('scroll')
+//     : navigation.classList.remove('scroll')
+// }
+// -----------------------------
+// Código que o Bruno me passou
+$(window).on('scroll', function () {
+  if ($(window).scrollTop() > 0) {
+    $('#navigation').addClass('scroll')
+  } else {
+    $('#navigation').removeClass('scroll')
+  }
+})
 
 function openMenu() {
   document.body.classList.add('menu-expanded')
@@ -11,3 +22,16 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`#home, 
+#home img, 
+#home .status, 
+#services,
+#services header,
+#services .cards,
+#about header,
+#about .content`)
